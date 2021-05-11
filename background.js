1ca5dcdcf278233fe8cc6c775e93b46d57cfe0f5
GanidhuAbey <ganidhu.abey@gmail.com>
2021-05-11 12:14:38 -0700

    update to manifest v 3

--- background.js
+++ background.js
@@ -1,11 +1,6 @@
-// background.js
+let color = '#3aa757';
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Send a message to the active tab
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    var activeTab = tabs[0];
-
-    //Open UI
-  });
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
 });
