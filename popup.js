20b3aed8e31c5c4739ca23e61f3ebac3be7e357b
GanidhuAbey <ganidhu.abey@gmail.com>
2021-05-11 15:03:32 -0700

    added google authentication

--- popup.js
+++ popup.js
@@ -1,2 +1,10 @@
 //this javascript file will send the due date and time required data from the user
 //to the storage, where it will be acessed by the background.js file.
+
+let event_name = document.getElementById("name");
+let due_date = document.getElementById("duedate");
+let time_needed = document.getElementById("time");
+
+chrome.storage.sync.set({ event_name });
+chrome.storage.sync.set({ due_date });
+chrome.storage.sync.set({ time_needed });
