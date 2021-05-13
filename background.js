a4b5bef5c783de514b3aeb7396d2a8e709e398ac
GanidhuAbey <ganidhu.abey@gmail.com>
2021-05-12 18:43:48 -0700

    now grab events from google

--- background.js
+++ background.js
@@ -1,10 +1,87 @@
-let color = '#3aa757';
+//chrome.identity.getAuthToken({ interactive: true });
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+var feeds = {};
 
-chrome.identity.getAuthToken({
-    interactive: true
-})
+feeds.CALENDAR_LIST_API_URL_ = 'https://www.googleapis.com/calendar/v3/users/me/calendarList';
+feeds.CALENDAR_EVENTS_API_URL_ = 'https://www.googleapis.com/calendar/v3/calendars/{calendarId}/events?'
+feeds.ids = [];
+
+feeds.test_array = ["cat", "bat", "nat"];
+
+feeds.requestInteracticeAuthToken = function() {
+    chrome.identity.getAuthToken({interactive: true}, function(token) {
+        if (chrome.runtime.lastError || !token) {
+            return;
+        }
+        feeds.fetchCalenders();
+        //feeds.fetchEvents();
+    })
+}
+
+/*
+feeds.fetchEvents = function() {
+    console.log(feeds.ids);
+    //fetch events of all calendars
+    chrome.identity.getAuthToken({interactive: false}, async function(token) {
+        var j;
+        console.log(feeds.ids[0]);
+        for (j = 0; j < feeds.ids.length; j++) {
+            fetch(feeds.CALENDAR_EVENTS_API_URL_.replace('{calendarId}', encodeURIComponent(feeds.ids[j])), {
+                headers: {
+                    'Authorization': 'Bearer ' + token,
+                }
+            })
+            .then(response => response.json())
+            .then(data => console.log(data));
+
+            console.log("hi");
+        }
+        console.log('5');
+    });
+    console.log('6');
+}
+*/
+
+
+feeds.fetchCalenders = function() {
+    chrome.identity.getAuthToken({interactive: false}, async function(token) {
+        awaitGetData(feeds.CALENDAR_LIST_API_URL_, token)
+            .then(data => {
+                var i;
+                console.log(data.items);
+                for (i = 0; i < data.items.length; i++) {
+                    awaitGetData(feeds.CALENDAR_EVENTS_API_URL_.replace('{calendarId}', encodeURIComponent(data.items[i].id)), token)
+                        .then(data => {
+                            console.log(data);
+                        });
+                }
+            });
+
+        //console.log(Object.keys(feeds.ids));
+
+
+        //console.log(feeds.test_array);
+        /*
+        var j;
+        for (j = 0; j < 3; j++) {
+            //console.log(feeds.ids[0]);
+            awaitGetData(feeds.CALENDAR_EVENTS_API_URL_.replace('{calendarId}', encodeURIComponent(feeds.ids[j])), token)
+                .then(data => {
+                    console.log(data);
+                });
+        }
+        */
+    })
+}
+
+feeds.requestInteracticeAuthToken();
+
+async function awaitGetData(url = '', token) {
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': 'Bearer ' + token,
+        }
+    })
+
+    return response.json();
+}
--- popup.js
+++ popup.js
@@ -1,10 +1,6 @@
 //this javascript file will send the due date and time required data from the user
 //to the storage, where it will be acessed by the background.js file.
 
-let event_name = document.getElementById("name");
-let due_date = document.getElementById("duedate");
-let time_needed = document.getElementById("time");
-
-chrome.storage.sync.set({ event_name });
-chrome.storage.sync.set({ due_date });
-chrome.storage.sync.set({ time_needed });
+document.getElementById('button').addEventListener("click", function() {
+    chrome.runtime.sendMessage({"message": "sign_in"});
+});
